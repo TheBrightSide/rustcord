dbb57bae8ca64138f539dfc4b5c37631be45f5d9
TheBrightSide <alexmanov8@gmail.com>
2021-07-18 22:54:15 +0300

    Moving from my library to serenity Add serenity to deps Remove logging from deps

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-pub mod discord;
-
-use discord::client::GatewayClient;
-use log::{info};
 use std::thread;
 use std::sync::mpsc;
-use std::time::Duration;
+use serenity::client::ClientBuilder;
 
 fn main() {
-    env_logger::init();
-    info!("starting threads...");
-    
-    let client = /* std::boxed::Box::new( */ GatewayClient::new(String::from("AAAAAAAAAAA")) /* ) */;
-    
-    client.start_ws_worker().unwrap().join().unwrap();
+    let client = ClientBuilder::new("<token>");
 }
